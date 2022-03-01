002b9f2685de6bba7873133114784f2f35c09b63
lkarion <l.karion@web.de>
2022-03-01 17:28:53 +0100

    new method getRandomArray2Dim

--- src/com/company/randomNDimArray.java
+++ src/com/company/randomNDimArray.java
@@ -10,12 +10,24 @@ public class randomNDimArray {
         array2Dim[0] = getRandomArray(size0, 0, 10);
         array2Dim[1] = getRandomArray(size1, 0, 10);
         System.out.println(Arrays.toString(array2Dim[0])+"\n"+Arrays.toString(array2Dim[1]));
+        System.out.println(Arrays.deepToString(getRandomArray2Dim(10,0,100)));
+
     }
     public static int[] getRandomArray(int size, int min, int max) {
         int[] resArr = new int[size];
 
         for (int i = 0; i < size; i++) {
             resArr[i] = (int)(Math.random()*(max-min+1)+min);
+        }
+        return resArr;
+    }
+
+    public static int[][] getRandomArray2Dim (int maxLen, int min, int max){
+        int[][] resArr = new int[(int)(Math.random()*maxLen)][(int)(Math.random()*maxLen)];
+        for (int i = 0; i < resArr.length; i++) {
+            for (int j = 0; j < resArr[i].length; j++) {
+                resArr[i][j] = (int)(Math.random()*(max-min+1)+min);
+            }
         }
         return resArr;
     }
